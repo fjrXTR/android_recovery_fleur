3203917cbd9299219a674665b4a9637dab9736cc
fjrbutuhthr gass <fajarslebew31@gmail.com>
2023-05-07 20:40:04 +0000

    fleur: Set Architecture
    Signed-off-by: fjrbutuhthr gass <fajarslebew31@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,3 +9,37 @@ DEVICE_PATH := device/xiaomi/fleur
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
+
+# A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    system \
+    vendor \
+    product \
+    boot \
+    vbmeta_vendor \
+    vbmeta_system
+
+BOARD_USES_RECOVERY_AS_BOOT := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := 
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := fleur,miel
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := mt6781
+TARGET_NO_BOOTLOADER := true
